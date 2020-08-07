040b809d108dd31918b0dda7b0626ed4a206ea2a
Jun <wangzhen_tju@126.com>
2020-08-07 16:03:09 +0800

    springboot 自定义启动加载类

--- src/test/java/com/juno/house/HouseApplicationTests.java
+++ src/test/java/com/juno/house/HouseApplicationTests.java
@@ -1,13 +1,30 @@
 package com.juno.house;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.io.IOException;
 
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.util.EntityUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class HouseApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+	
+	@Autowired
+	private HttpClient httpClient;
 
+	@Test
+	public void testHttpClient() throws ClientProtocolException, IOException {
+		System.out.println(EntityUtils.toString(httpClient.execute(new HttpGet("https://www.baidu.com")).getEntity()));
+	}
 }
